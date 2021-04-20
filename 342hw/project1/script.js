3fa0402a25921bafd30485b0c141747ccecf2e74
Jay King <76966793+jayrking@users.noreply.github.com>
2021-04-20 10:26:10 -0700

    quote reveal added

--- 342hw/project1/script.js
+++ 342hw/project1/script.js
@@ -18,3 +18,13 @@ function windowClose() {
         }
         }
         })
+
+
+        function myFunction() {
+            var x = document.getElementById("hide");
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          }
